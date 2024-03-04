f94bfa204b69360ec4ed9b773332a48949614f2a
BernardasAdomas <bernardas.mickevicius@stud.vilniustech.lt>
2024-03-04 15:05:40 +0200

    added guideline-list component

---
+++ src/app/guideline-list/guideline-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guideline-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './guideline-list.component.html',
+  styleUrl: './guideline-list.component.css'
+})
+export class GuidelineListComponent {
+
+}
