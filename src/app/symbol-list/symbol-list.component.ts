f82151783e10f9693fdd602ca46da3ce17aa4b33
BernardasAdomas <bernardas.mickevicius@stud.vilniustech.lt>
2024-03-06 16:42:48 +0200

    added info to the simbols route

---
+++ src/app/symbol-list/symbol-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-symbol-list',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './symbol-list.component.html',
+  styleUrl: './symbol-list.component.css'
+})
+export class SymbolListComponent {
+
+}
