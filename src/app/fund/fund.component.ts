3386750a6bf2ab4065ce22a8efe30291f5e93853
BernardasAdomas <bernardas.mickevicius@stud.vilniustech.lt>
2024-03-07 11:37:39 +0200

    added fund route

---
+++ src/app/fund/fund.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-fund',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './fund.component.html',
+  styleUrl: './fund.component.css'
+})
+export class FundComponent {
+
+}
