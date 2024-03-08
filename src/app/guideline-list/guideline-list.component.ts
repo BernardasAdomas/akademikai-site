aabfe6048dc17114ac574a795ea6f4fa18cb129b
BernardasAdomas <bernardas.mickevicius@stud.vilniustech.lt>
2024-03-08 15:29:30 +0200

    styled guideline-list component somewhat

--- src/app/guideline-list/guideline-list.component.ts
+++ src/app/guideline-list/guideline-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-guideline-list',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './guideline-list.component.html',
   styleUrl: './guideline-list.component.css'
 })
