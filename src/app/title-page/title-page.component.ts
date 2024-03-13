97293e69b74eb5094c42d81b990c4e290d2feb23
BernardasAdomas <bernardas.mickevicius@stud.vilniustech.lt>
2024-03-13 16:13:39 +0200

    added gpm component

--- src/app/title-page/title-page.component.ts
+++ src/app/title-page/title-page.component.ts
@@ -3,11 +3,12 @@ import { CalendarComponent } from '../calendar/calendar.component';
 import { DidYouKnowComponent } from '../did-you-know/did-you-know.component';
 import { SurveyComponent } from '../survey/survey.component';
 import { NewsListComponent } from '../news-list/news-list.component';
+import { IncomeTaxComponent } from '../income-tax/income-tax.component';
 
 @Component({
   selector: 'app-title-page',
   standalone: true,
-  imports: [CalendarComponent, DidYouKnowComponent, SurveyComponent, NewsListComponent],
+  imports: [CalendarComponent, DidYouKnowComponent, SurveyComponent, NewsListComponent, IncomeTaxComponent],
   templateUrl: './title-page.component.html',
   styleUrl: './title-page.component.css'
 })
